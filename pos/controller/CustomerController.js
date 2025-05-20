94f28104c4599ded7bc06f4c24c8b750e44d7716
thenurisenethma <thenurisenethma@gmail.com>
2025-05-20 23:23:43 +0530

    load customer

--- pos/controller/CustomerController.js
+++ pos/controller/CustomerController.js
@@ -2,6 +2,24 @@ import {customers_db, items_db,orders_db} from '../db/DB.js';
 
 import CustomerModel from "../model/CustomerModel.js";
 
+function loadCustomers() {
+    $('#customer-tbody').empty();
+    customers_db.map((item, index) => {
+        let custId = item.custId;
+        let name = item.name;
+        let address = item.address;
+        let salary = item.salary;
+
+        let data = `<tr>
+                            <td>${index + 1}</td>
+                            <td>${custId}</td>
+                            <td>${name}</td>
+                            <td>${address}</td>
+                            <td>${salary}</td>
+                        </tr>`
+        $('#customer-tbody').append(data);
+    })
+}
 
     $('#customer_save').on('click', function () {
         let custId = $('#custId').val();

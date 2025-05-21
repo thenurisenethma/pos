d569cfcf561776cf3ef94317e693cd4f45e22237
thenurisenethma <thenurisenethma@gmail.com>
2025-05-21 11:42:20 +0530

    delete item in ItemController

--- pos/model/ItemModel.js
+++ pos/model/ItemModel.js
@@ -5,5 +5,12 @@ export default class ItemModel {
         this.price = price;
         this.qty = qty;
     }
-
+    deleteItem(id, items) {
+        const index = items.findIndex(c => c.itemCode === id);
+        if (index !== -1) {
+            items.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
 }

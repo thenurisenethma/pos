4ec2048d99af0a3106d345eb78daf1e35ac0b6ce
thenurisenethma <thenurisenethma@gmail.com>
2025-05-23 19:56:24 +0530

    update order fields

--- pos/controller/OrderController.js
+++ pos/controller/OrderController.js
@@ -123,4 +123,11 @@ function clearFields() {
     $('#orderItemCode').val('');
     $('#orderPrice').val('');
     $('#orderQty').val('');
+
+    $("#custName, #address, #salary, #date, #orderItemCode, #orderPrice, #orderQty, #qtyOnHand, #orderId").on('input change', function () {
+        $(this).css({
+            "border": "",
+            "background": ""
+        });
+    });
 }
